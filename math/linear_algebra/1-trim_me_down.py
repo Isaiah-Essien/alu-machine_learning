1161e58307284a6b2a9baab760e39b4f1f222f97
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-05-13 15:41:08 +0200

    .\1-trim_me_down.py

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -2,5 +2,5 @@
 '''This code prints slices the 3rd and fourth column of the larger matrixs into a 2D matrices'''
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle=[]
-the_middle = [row[2:4]for row in matrix]
+the_middle.append([row[2:4]for row in matrix])
 print("The middle columns of the matrix are: {}".format(the_middle))
