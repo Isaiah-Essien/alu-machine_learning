6a30d86f91915aafdcd16ec6db882cf9209e153b
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-05-20 21:21:07 +0200

    .\0-determinant.py

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -28,7 +28,7 @@ def determinant(matrix):
     for row in matrix:
         if type(row) is not list:
             raise TypeError("matrix must be a list of lists")
-        if matrix==[[]]:
+        if matrix == [[]]:
             return 1
         if len(row) != matrix_size:
             raise ValueError("matrix must be a square matrix")
