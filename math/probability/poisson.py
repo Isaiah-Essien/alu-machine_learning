875d85e2275a82badc91aaaaa3209aadf781d9e2
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-06-07 00:47:30 +0200

    task 0-initialize Poisson

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
+"""
+This module defines a Poisson class for representing and
+manipulating Poisson distributions.
+"""
 class Poisson:
     '''
-    This class calculates the function of a lambtha,
-    checking if the data is greater than 2,
-    and if lambtha is greater than 0
+    Represents a Poisson distribution.
+
+    Attributes:
+        lambtha (float): The rate (λ) of the distribution, representing
+        the expected number of occurrences in a given
+        time frame
     '''
     def __init__(self, data=None, lambtha=1.):
-        '''This function initializes lambtha'''
+        '''
+        Initializes the Poisson distribution with data or a given λ.
+
+        Args:
+            data (list, optional): List of the data to be used to estimate the distribution.
+            lambtha (float, optional): The expected number of occurrences in a given time frame.
+
+        Raises:
+            ValueError: If lambtha is not a positive value.
+            TypeError: If data is not a list.
+            ValueError: If data does not contain multiple values
+        '''
         # Validate lambtha
         if lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
