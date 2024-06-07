229c8013688cc7d8b948a2b36631edf8990e8d08
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-06-07 02:11:03 +0200

    task 3- exponential CDF

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -3,6 +3,7 @@
 '''
 This module defines an exponential distribution
 '''
+e = 2.7182818285
 
 
 class Exponential:
@@ -42,6 +43,22 @@ class Exponential:
             # Calculate lambtha from data
             self.lambtha = float(1 / (sum(data) / len(data)))
 
+    def pdf(self, x):
+        '''
+        Calculates the value of the PDF for a given time period.
+
+        Args:
+            x (float): The time period.
+
+        Returns:
+            float: The PDF value for x.
+        '''
+        if x < 0:
+            return 0
+        # Calculate PDF using the formula
+        pdf_value = self.lambtha * e ** (-self.lambtha * x)
+        return pdf_value
+
     def cdf(self, x):
         '''
         Calculates the value of the CDF for a given time period.
@@ -55,5 +72,5 @@ class Exponential:
         if x < 0:
             return 0
         # Calculate e^(-λx) using the provided value of e
-        e = 2.7182818285 ** (-self.lambtha * x)
-        return 1 - e
+        e_neg_lambda_x = e ** (-self.lambtha * x)
+        return 1 - e_neg_lambda_x
