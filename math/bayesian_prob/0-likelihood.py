ede198ed300ced4b1345e93b05a0abf06a2543d8
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-06-21 00:42:23 +0200

    Liklihood

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-'''Script for Calculating the liklihood prob'''
+'''Script'''
+
 import numpy as np
 
 
@@ -25,6 +26,7 @@ def binomial_coefficient(n, k):
         c = c * (n - i) // (i + 1)
     return c
 
+
 def likelihood(x, n, P):
     """
     Calculates the likelihood of observing the data,
@@ -44,20 +46,30 @@ def likelihood(x, n, P):
     """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    
+
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
-    
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+
     if x > n:
         raise ValueError("x cannot be greater than n")
-    
+
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    
+
     if np.any((P < 0) | (P > 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
     coeff = binomial_coefficient(n, x)
     likelihoods = coeff * (P ** x) * ((1 - P) ** (n - x))
-    
+
     return likelihoods
+
+
+if __name__ == '__main__':
+    import numpy as np
+    likelihood = __import__('0-likelihood').likelihood
+
+    # [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    P = np.linspace(0, 1, 11)
+    print(likelihood(26, 130, P).round(12))
