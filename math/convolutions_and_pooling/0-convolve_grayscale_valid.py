4af6eb0c92fccf3d403262a81ad684ea904e5aa0
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-06-25 14:23:39 +0200

    Task-o valid convolution

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -17,11 +17,12 @@ def convolve_grayscale_valid(images, kernel):
         kw is the width of the kernel
     '''
     m, h, w = images.shape
-    kw , kh = kernel.shape
+    kw, kh = kernel.shape
     new_h = h - kh + 1
     new_w = w - kw + 1
     convolved_images = np.zeros((m, new_h, new_w))
     for i in range(new_h):
         for j in range(new_w):
-            convolved_images[:, i, j] = np.sum(images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+            convolved_images[:, i, j] = np.sum(
+                images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
     return np.array(convolved_images)
