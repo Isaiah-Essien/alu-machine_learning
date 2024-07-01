c500ead6c8e83da3bf47b235777ee8aeced514cc
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-07-01 14:41:14 +0200

    task5

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-"""
-    A function def
+'''
+    a function def
     convolve(images, kernels, padding='same', stride=(1, 1)):
     that performs a convolution on images using multiple kernels:
-"""
+'''
 
 
 import numpy as np
 
 
-def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+def convolve(images, kernels, padding='same', stride=(1, 1)):
     """
     Performs a convolution on images with multiple channels
     using given padding and stride
@@ -26,6 +26,7 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
             kh: height of the kernel
             kw: width of the kernel
             c: number of channels in the image
+            nc:  is the number of kernels
         padding [tuple of (ph, pw) or 'same' or 'valid']:
             ph: padding for the height of the images
             pw: padding for the width of the images
@@ -42,12 +43,12 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
         numpy.ndarray contained convolved images
     """
     m, height, width, c = images.shape
-    kh, kw, kc = kernel.shape
+    kh, kw, kc, nc = kernels.shape
     sh, sw = stride
-    if padding == 'same':
+    if padding is 'same':
         ph = ((((height - 1) * sh) + kh - height) // 2) + 1
         pw = ((((width - 1) * sw) + kw - width) // 2) + 1
-    elif padding == 'valid':
+    elif padding is 'valid':
         ph = 0
         pw = 0
     else:
@@ -56,12 +57,14 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
                     'constant', constant_values=0)
     ch = ((height + (2 * ph) - kh) // sh) + 1
     cw = ((width + (2 * pw) - kw) // sw) + 1
-    convoluted = np.zeros((m, ch, cw))
+    convoluted = np.zeros((m, ch, cw, nc))
 
-    for i, h in enumerate(range(0, (height + (2 * ph) - kh + 1), sh)):
-        for j, w in enumerate(range(0, (width + (2 * pw) - kw + 1), sw)):
-            output = np.sum(images[:, h: h + kh, w: w + kw, :] * kernel,
-                            axis=(1, 2, 3))
-            convoluted[:, i, j] = output
+    for index in range(nc):
+        kernel_index = kernels[:, :, :, index]
+        for i, h in enumerate(range(0, (height + (2 * ph) - kh + 1), sh)):
+            for j, w in enumerate(range(0, (width + (2 * pw) - kw + 1), sw)):
+                output = np.sum(images[:, h: h + kh, w: w + kw, :]
+                                * kernel_index, axis=(1, 2, 3))
+                convoluted[:, i, j, index] = output
 
     return convoluted
