95e515d672283a270d4b93642c8e9ba67b15620b
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-09-12 16:43:31 +0200

    task 8

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -32,7 +32,7 @@ class NeuralNetwork:
         self.nx = nx
         self.nodes = nodes
         self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros(1, nodes)
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
