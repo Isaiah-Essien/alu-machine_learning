a326dbe18513b41649c3e08436f16c3291bfe363
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-10-25 19:25:06 +0200

    main.py

--- pipeline/apis/0-main.py
+++ pipeline/apis/0-main.py
@@ -1,40 +1,8 @@
 #!/usr/bin/env python3
 """
-    method that returns the list of ships
-    that can hold a given number of passengers:
+Test file
 """
-
-
-import requests
-
-
-def availableShips(passengerCount):
-    """
-    method that returns the list of ships
-    that can hold a given number of passengers:
-
-    Args:
-        passengerCount (int): number of passengers
-        to be transported
-        Add pagination to the data
-    Returns:
-        list of ships that can hold the given
-    """
-
-    url = "https://swapi-api.alx-tools.com/api/starships/"
-    ships = []
-    while url:
-        response = requests.get(url)
-        data = response.json()
-        for ship in data["results"]:
-            if (
-                ship["passengers"] != "n/a"
-                and ship["passengers"] != "unknown"
-                and ship["passengers"] != "0"
-                and ship["passengers"] != "none"
-            ):
-                ship["passengers"] = ship["passengers"].replace(",", "")
-                if int(ship["passengers"]) >= passengerCount:
-                    ships.append(ship["name"])
-        url = data["next"]
-    return ships
+availableShips = __import__('0-passengers').availableShips
+ships = availableShips(4)
+for ship in ships:
+    print(ship)
