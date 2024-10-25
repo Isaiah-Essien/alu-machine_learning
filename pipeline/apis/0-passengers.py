bf072c00a5080565232d5688ba78308436466164
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-10-25 11:12:37 +0200

    task0

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-'''This script collects data from the swapiapi'''
+"""
+    method that returns the list of ships
+    that can hold a given number of passengers:
+"""
 
 
 import requests
@@ -7,45 +10,31 @@ import requests
 
 def availableShips(passengerCount):
     """
-    Method that returns the list of ships
-    that can hold a given number of passengers.
+    method that returns the list of ships
+    that can hold a given number of passengers:
 
     Args:
         passengerCount (int): number of passengers
         to be transported
-
+        Add pagination to the data
     Returns:
-        list: List of ship names that can hold
-        the given number of passengers
+        list of ships that can hold the given
     """
-    url_link = 'https://swapi-api.alx-tools.com/api/starships/'
-    list_ships = []
 
-    while url_link:
-        response = requests.get(url_link)
-
-        # Handle error incase request fails
-        if response.status_code != 200:
-            print(f"Failed to retrieve data:
-                  {response.status_code}")
-            break
-
+    url = "https://swapi-api.alx-tools.com/api/starships/"
+    ships = []
+    while url:
+        response = requests.get(url)
         data = response.json()
-
-        for ship in data['results']:
-            # Skip ships with invalid passenger values
-            if ship["passengers"] not in [
-                "n/a", "unknown", "0", "none"]:
-                try:
-                    ship["passengers"] = ship[
-                        "passengers"].replace(",", "")
-                    if int(ship['passengers']) >= passengerCount:
-                        list_ships.append(ship['name'])
-                except ValueError:
-                    # Handle unexpected non-numeric passenger values
-                    pass
-
-        # Move to the next page if available
-        url_link = data['next']
-
-    return list_ships
+        for ship in data["results"]:
+            if (
+                ship["passengers"] != "n/a"
+                and ship["passengers"] != "unknown"
+                and ship["passengers"] != "0"
+                and ship["passengers"] != "none"
+            ):
+                ship["passengers"] = ship["passengers"].replace(",", "")
+                if int(ship["passengers"]) >= passengerCount:
+                    ships.append(ship["name"])
+        url = data["next"]
+    return ships
