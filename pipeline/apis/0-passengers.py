c143d55ba8f4213120a502d21672008325e471ce
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-10-25 11:10:09 +0200

    task0

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+'''This script collects data from the swapiapi'''
+
 
 import requests
-'''This script collects data from the swapiapi'''
+
 
 def availableShips(passengerCount):
     """
