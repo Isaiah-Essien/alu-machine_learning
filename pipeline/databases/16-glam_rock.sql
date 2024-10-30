d3792fb71981c45958377e763fb94ac897bea8cc
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-10-31 01:45:27 +0200

    tasks 10-34

---
+++ pipeline/databases/16-glam_rock.sql
@@ -0,0 +1,5 @@
+--Lists all bands with glam rock
+SELECT band_name, IF(split IS NULL, (2020 - formed), (split - formed)) AS lifespan
+       FROM metal_bands
+       WHERE `style` LIKE '%Glam rock%'
+       ORDER BY lifespan DESC;
