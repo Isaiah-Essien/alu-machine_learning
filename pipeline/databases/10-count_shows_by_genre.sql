a58d9f50f5ef2290a57fa382cf993e0e7040d1a2
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-10-31 00:30:56 +0200

    tasks 1-10

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- List all genres and the number of shows linked to each, ordered by show count descending
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING number_of_shows > 0
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,5 +1,10 @@
 -- Display the average temperature by city, ordered by temperature in descending order
-SELECT city, AVG(temperature) AS average_temperature
-FROM your_table_name
-GROUP BY city
-ORDER BY average_temperature DESC;
+SELECT
+    city,
+    AVG(value) AS avg_temp
+FROM
+    temperatures
+GROUP BY
+    city
+ORDER BY
+    avg_temp DESC;
